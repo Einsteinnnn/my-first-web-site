8beed70134cb928631b42c801f772d7bda81fec0
Einsteinnnn <105585584+Einsteinnnn@users.noreply.github.com>
2022-07-27 05:07:18 +0900

    Add files via upload

---
+++ syntax/Templates.js
@@ -0,0 +1,9 @@
+var name = 'k8805';
+var letter = 'Dear '+name+ '\n\nLorem ipsum dolor sit amet consecture adipsieiflkd and you amekd a wake so we make show me the money and show we makes so ride a bike Every professor include that annoying kk'+name;
+console.log(letter);
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet consecture adipsieiflkd and you amekd a wake so we make show me the money and show we makes so ride a bike Every professor include that annoying kk.`;
+
+console.log(letter);
---
+++ syntax/array.js
@@ -0,0 +1,2 @@
+var arr = ['a','b','c','d'];
+console.log(arr[1],arr[2])
